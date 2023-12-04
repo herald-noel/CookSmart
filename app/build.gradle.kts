bb9117717c12db4082b67a8fa8ae119964ef4c98
herald-noel <bontilao.heraldnoel@gmail.com>
2023-12-04 19:46:21 +0800

    Add picasso, and retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -51,4 +51,9 @@ dependencies {
 
     // TFLite
     implementation("org.tensorflow:tensorflow-lite-task-vision:0.3.1")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Picasso
+    implementation ("com.squareup.picasso:picasso:2.8")
 }
