1897a34d93b295d3520a311835a345f14648e26f
herald-noel <bontilao.heraldnoel@gmail.com>
2023-12-07 02:57:29 +0800

    Add onClickListener on recipe fragment

--- app/src/main/java/com/example/cooksmart/fragments/RecipeFragment.kt
+++ app/src/main/java/com/example/cooksmart/fragments/RecipeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cooksmart.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -13,14 +14,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.cooksmart.Ingredient
 import com.example.cooksmart.IngredientFragmentListener
 import com.example.cooksmart.R
+import com.example.cooksmart.ViewRecipeActivity
 import com.example.cooksmart.adapter.RecipeAdapter
 import com.example.cooksmart.api.RequestManager
 import com.example.cooksmart.api.listener.RecipeResponseListener
 import com.example.cooksmart.api.model.RecipeApiResponse
+import com.example.cooksmart.api.model.RecipeApiResponseItem
 import com.example.cooksmart.utils.ListToCommaSeparate
 
 class RecipeFragment(ingredientList: ArrayList<Ingredient>) : Fragment(),
-    IngredientFragmentListener {
+    IngredientFragmentListener, RecipeAdapter.OnClickListener {
     private var ingredientList: MutableList<Ingredient> = ingredientList
     private lateinit var recipeAdapter: RecipeAdapter
     private var lastIngredients: String? = null
@@ -40,7 +43,7 @@ class RecipeFragment(ingredientList: ArrayList<Ingredient>) : Fragment(),
         val recyclerview: RecyclerView = view.findViewById(R.id.recycler_recipe_home)
         recyclerview.layoutManager = LinearLayoutManager(requireContext())
 
-        recipeAdapter = RecipeAdapter(RecipeApiResponse())
+        recipeAdapter = RecipeAdapter(RecipeApiResponse(), this)
         recyclerview.adapter = recipeAdapter
         if (ingredients != lastIngredients) {
             lastIngredients = ingredients
@@ -69,5 +72,12 @@ class RecipeFragment(ingredientList: ArrayList<Ingredient>) : Fragment(),
 
         val ingredients = ListToCommaSeparate.convertToString(updatedIngredients)
         managerGetRecipe(ingredients)
+    }
+
+    override fun onRecipeClick(recipeItem: RecipeApiResponseItem) {
+        val intent = Intent(requireContext(), ViewRecipeActivity::class.java)
+        intent.putExtra("recipeName", recipeItem.title)
+        intent.putExtra("recipeImageUrl", recipeItem.image)
+        startActivity(intent)
     }
 }
