716c75c5f86181c67cfea5c1107cbe52c38e98b6
Canete, Dominic <120698492+CanetDominic@users.noreply.github.com>
2023-12-10 00:07:05 +0800

    add splashcreen and icon

--- app/src/main/java/com/example/cooksmart/MainActivity.kt
+++ app/src/main/java/com/example/cooksmart/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.cooksmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/cooksmart/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.cooksmart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                startActivity(new Intent(SplashScreenActivity.this,LoginActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
