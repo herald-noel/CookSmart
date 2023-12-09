127fb1d93bcad52cdbaa4985ea49075b08a2d361
herald-noel <bontilao.heraldnoel@gmail.com>
2023-12-10 01:56:13 +0800

    Lower splashscreen delay

--- app/src/main/java/com/example/cooksmart/SplashScreenActivity.java
+++ app/src/main/java/com/example/cooksmart/SplashScreenActivity.java
@@ -19,6 +19,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashScreenActivity.this,LoginActivity.class));
                 finish();
             }
-        }, 3000);
+        }, 1500);
     }
 }
