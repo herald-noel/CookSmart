5334f3061465514c705258655535f657d5a6b6ef
herald-noel <bontilao.heraldnoel@gmail.com>
2023-12-10 18:08:26 +0800

    Add favorite recipe thumbnail

--- app/src/main/java/com/example/cooksmart/controller/profile/ProfileController.kt
+++ app/src/main/java/com/example/cooksmart/controller/profile/ProfileController.kt
@@ -3,34 +3,39 @@ package com.example.cooksmart.controller.profile
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.example.cooksmart.FavoriteRecipeActivity
 import com.example.cooksmart.HomeActivity
 import com.example.cooksmart.LoginActivity
 import com.example.cooksmart.ProfileActivity
 import com.example.cooksmart.controller.base.Controller
+import com.example.cooksmart.data.Recipe
 import com.example.cooksmart.model.profile.IProfileCallback
 import com.example.cooksmart.model.profile.ProfileModel;
 import com.example.cooksmart.model.recipe.RecipeModel
-import com.example.cooksmart.view.profile.IProfileView
+import com.example.cooksmart.view.profile.ProfileView
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 
 class ProfileController(
     private val profileModel: ProfileModel,
-    private val profileView: IProfileView
-) : Controller(), IProfileController{
+    private val profileView: ProfileView
+) : Controller(), IProfileController {
+    val favoriteRecipes: ArrayList<Recipe> = ArrayList()
 
     val mAuth = FirebaseAuth.getInstance()
     val currentUserId = FirebaseAuth.getInstance().currentUser
     val uID = currentUserId?.getUid()
     val reference = FirebaseDatabase.getInstance().getReference("users").child(uID.orEmpty())
 
-    private val callback = object: IProfileCallback{
+    private val callback = object : IProfileCallback {
         override fun onProfileReceived(profile: ProfileModel) {
-           profileView.updateProfile(profileModel)
+            profileView.updateProfile(profileModel)
         }
 
         override fun onProfileError(error: String) {
@@ -38,10 +43,12 @@ class ProfileController(
         }
     }
 
-    override fun displayPastRecipe(){
+    override fun displayPastRecipe() {
     }
-    override fun FavoriteRecipe(){
+
+    override fun FavoriteRecipe() {
     }
+
     override fun displayProfileDetails() {
         reference.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -58,6 +65,7 @@ class ProfileController(
                                 }
                             }
                         }
+
                         "favoriteRecipes" -> {
                             for (recipeSnapshot in userSnapshot.child("favoriteRecipes").children) {
                                 val recipe = recipeSnapshot.getValue(RecipeModel::class.java)
@@ -70,16 +78,19 @@ class ProfileController(
                 }
                 callback.onProfileReceived(profileModel)
             }
+
             override fun onCancelled(databaseError: DatabaseError) {
                 // Handle errors here
             }
         })
     }
-    fun redirectToHome(context: Context){
+
+    fun redirectToHome(context: Context) {
         val intent = Intent(context, HomeActivity::class.java)
         context.startActivity(intent)
     }
-    fun redirectToProfile(context: Context){
+
+    fun redirectToProfile(context: Context) {
         val intent = Intent(context, ProfileActivity::class.java)
         context.startActivity(intent)
     }
@@ -94,8 +105,52 @@ class ProfileController(
         mAuth.signOut()
     }
 
-    fun showFavoriteRecipes(context: Context) {
-       val intent = Intent(context, FavoriteRecipeActivity::class.java)
+    fun redirectFavoriteRecipes(context: Context) {
+        val intent = Intent(context, FavoriteRecipeActivity::class.java)
         context.startActivity(intent)
+    }
+
+    fun showFavoriteRecipesThumbnail() {
+        val mAuth = FirebaseAuth.getInstance()
+        val uid = mAuth.uid
+        val databaseReference = Firebase.database.reference
+
+        uid?.let {
+            val recipeHistoryRef =
+                databaseReference.child("users").child(it).child("recipeFavorites")
+            recipeHistoryRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    // Iterate through the snapshot to get recipeIds
+                    var ctr = 0
+                    for (childSnapshot in snapshot.children) {
+                        if (ctr > 2) {
+                            break;
+                        }
+                        ctr++
+                        getSnapShot(childSnapshot)
+                    }
+                    if (favoriteRecipes.size != 0) {
+                        profileView.showFavoriteRecipes(favoriteRecipes)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("Recipe History", "Error retrieving recipe history: ${error.message}")
+                }
+            })
+
+        }
+    }
+
+    fun getSnapShot(childSnapshot: DataSnapshot) {
+        val recipeId = childSnapshot.child("id").getValue(Int::class.java) ?: 0
+        val name = childSnapshot.child("title").getValue(String::class.java) ?: ""
+        val imgUrl =
+            childSnapshot.child("image").getValue(String::class.java) ?: ""
+        val imgType =
+            childSnapshot.child("imageType").getValue(String::class.java) ?: ""
+        val recipe = Recipe(recipeId, name, imgUrl, imgType)
+        Log.d("RECIPE", "$recipeId, $name, $imgUrl, $imgType")
+        favoriteRecipes.add(recipe)
     }
 }
--- app/src/main/java/com/example/cooksmart/view/profile/ProfileView.kt
+++ app/src/main/java/com/example/cooksmart/view/profile/ProfileView.kt
@@ -6,14 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.cooksmart.R
 
 import com.example.cooksmart.controller.profile.ProfileController
+import com.example.cooksmart.data.Recipe
 import com.example.cooksmart.model.profile.ProfileModel
 import com.example.cooksmart.view.base.CView
+import com.squareup.picasso.Picasso
 
-class ProfileView (private val context: Context,viewGroup: ViewGroup?) : CView(), IProfileView {
+class ProfileView(private val context: Context, viewGroup: ViewGroup?) : CView(), IProfileView {
 
     override val view: View
     override var model: ProfileModel
@@ -26,6 +29,11 @@ class ProfileView (private val context: Context,viewGroup: ViewGroup?) : CView()
 
     private var emailTV: TextView
     private var bdTV: TextView
+
+    private var favImageView1: ImageView
+    private var favImageView2: ImageView
+    private var favImageView3: ImageView
+
     init {
         model = ProfileModel()
         controller = ProfileController(model, this)
@@ -39,36 +47,43 @@ class ProfileView (private val context: Context,viewGroup: ViewGroup?) : CView()
         emailTV = view.findViewById(R.id.profileName2)
         bdTV = view.findViewById(R.id.profileName3)
 
+        favImageView1 = view.findViewById(R.id.favImageView1)
+        favImageView2 = view.findViewById(R.id.favImageView2)
+        favImageView3 = view.findViewById(R.id.favImageView3)
+
         controller.displayProfileDetails()
 
         backButtonListener()
         logoutButtonListener()
         favoritesButtonListener()
         historyButtonListener()
+        initFavoriteRecipesThumbnail()
     }
 
     override fun getRootView(): View {
         return view
     }
 
-    private fun logoutButtonListener(){
-        logout.setOnClickListener(){
+    private fun logoutButtonListener() {
+        logout.setOnClickListener {
             controller.logout(context)
         }
     }
-    private fun favoritesButtonListener(){
+
+    private fun favoritesButtonListener() {
         favoriteBtn.setOnClickListener {
-            controller.showFavoriteRecipes(context)
+            controller.redirectFavoriteRecipes(context)
         }
     }
-    private fun historyButtonListener(){
+
+    private fun historyButtonListener() {
         historyBtn.setOnClickListener {
 
         }
     }
 
-    private fun backButtonListener(){
-        backbutton.setOnClickListener(){
+    private fun backButtonListener() {
+        backbutton.setOnClickListener {
             controller.redirectToHome(context)
         }
     }
@@ -78,4 +93,19 @@ class ProfileView (private val context: Context,viewGroup: ViewGroup?) : CView()
         emailTV.text = model.email
         bdTV.text = model.birthdate
     }
+
+    private fun initFavoriteRecipesThumbnail() {
+        controller.showFavoriteRecipesThumbnail()
+    }
+
+    fun showFavoriteRecipes(favoriteRecipes: ArrayList<Recipe>) {
+        val imgViews: ArrayList<ImageView> = arrayListOf(favImageView1, favImageView2, favImageView3)
+        for ((index, recipe) in favoriteRecipes.withIndex()) {
+            Picasso.get()
+            .load(recipe.image)
+            .into(imgViews[index])
+        }
+    }
+
+
 }
